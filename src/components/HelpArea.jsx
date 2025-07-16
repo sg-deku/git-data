809bbd8f816bc9ab38390034cc830991735c7c91
sg-deku <sghosh92de@gmail.com>
2025-07-16 17:32:44 +0200

    added upload caching

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -15,7 +15,7 @@ const HELP_ITEMS = [
   {
     id: 'h3',
     title: 'What is GitHub Actions?',
-    text: 'GitHub Actions is an automation service (or CI / CD service) that helps you automate repository-related workflows and processes.',
+    text: 'GitHub Actions is an automation ser vice (or CI / CD service) that helps you automate repository-related workflows and processes.',
   },
 ];
 
